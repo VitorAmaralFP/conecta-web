9c1dc93dc85aaefdf573fab053895354644c7933
VitorAmaralFP <pereira.vitor@edu.fecap.br>
2024-11-22 18:34:51 -0300

    g

--- index.js
+++ index.js
@@ -31,6 +31,9 @@ const sessionStore = new MySQLStore({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false, // Ignora a verificação de certificado
+    },
 });
 
 app.use(
